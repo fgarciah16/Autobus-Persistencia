1ca6be75be55fd9f6a87794f286bbb5ee7312dea
fgarciah16 <fgarciahernandez2@gmail.com>
2020-06-20 10:52:40 -0500

    Base

---
+++ GestionAsientosAutobuses/Datos base de datos.sql
@@ -0,0 +1,44 @@
+INSERT INTO AUTOBUS (TIPO,ASIENTOS) VALUES (Basico,36),
+                                           (Lujo,36),
+                                           (Mini,24),
+                                           (Primera,36),
+                                           (Economico,40);
+                                      
+                                          
+
+
+INSERT INTO CORRIDAS (ORIGEN,DESTINO,AUTOBUS,FECHA_HORA,COSTO) VALUES (11,13,3,2020-06-19,340),
+                                                                     (14,11,4,2020-06-25,544),
+                                                                     (13,15,4,2020-06-25,254),	
+	                                                                 (17,18,3,2020-06-27,488),
+	                                                                 (15,16,2,2020-06-27,254),
+	                                                                 (17,14,1,2020-06-22,584),
+                                                                     (12,15,3,2020-06-26,2584),
+	                                                                 (16,11,4,2020-06-17,44545),
+                                                                     (18,15,2,2020-06-21,3248),
+	                                                                 (14,15,4,2020-06-26,650),
+	                                                                 (11,18,3,2020-06-20,850),
+                                                                     (15,18,11,2020-06-22,584);
+
+
+
+INSERT INTO MUNICIPIO (NOMBRE,HABITANTES) VALUES (Oaxaca,25857),
+                                                 (Veracruz,46446),
+                                                 (Puebla,5445),
+                                                 (Sonora,8787),
+                                                 (Toluca,87878),
+                                                 (Mexico DF,122452),
+                                                 (Chiapas,124545),
+                                                 (Tijuana,54545);
+                                                 
+
+
+INSERT INTO POBLACION (Oaxaca,1),
+                      (Veracruz,2),
+                      (Puebla,3),
+                      (Sonora,4),
+                      (Toluca,5),
+                      (Mexico DF,6),
+                      (Chiapas,8),
+                      (Tijuana,8);
+                      
